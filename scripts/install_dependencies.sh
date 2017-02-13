3d9b68b0999fe4f3c563c41457511715acb2af07
mad <madhavi@gmail.com>
2017-02-13 20:56:30 +0530

    modified install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 apt-get update
 apt-get install -y apache2
+pip install --upgrade pip
+echo "upgraded pip"
 pip install pybuilder
+echo "installed pybuilder"
 pyb install_dependencies
+echo "installed dependencies"
 
